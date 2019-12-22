c9ade5740b815403fba48661dca18386d8d5d70a
carlosnumber9 <charlie_theboss023@hotmail.com>
2019-12-23 00:47:43 +0100

    Creating component files

---
+++ src/app/fragments/item-search-bar/item-search-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-search-bar',
+  templateUrl: './item-search-bar.component.html',
+  styleUrls: ['./item-search-bar.component.css']
+})
+export class ItemSearchBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
